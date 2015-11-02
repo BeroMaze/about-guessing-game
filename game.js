c15972f7fba3b3ed593c2826b9f28b71b8551c89
BeroMaze <Berning.corey@gmail.com>
2015-11-01 22:17:00 -0800

    added in functions

--- game.js
+++ game.js
@@ -1,49 +1,71 @@
-var game = prompt("Would you like to learn a little about me?");
 var right = 0;
-  if ((game === "yes") || (game === "Yes") || (game === "Y") || (game === "y") || (game === "YES")) {
-      alert("Lets play a game. I will ask you three questions about me. See how many you can get right.");
+var userYes = function(resp) {
+      if ((resp === "yes") || (resp === "Yes") || (resp === "y") || (resp === "Y") || (resp === "YES"))  {
+        right++;
+        console.log(right);
+      }
+    }
+var userNo = function(resp){
+      if ((resp === "no") || (resp === "No") || (resp === "n") || (resp === "N") || (resp === "NO")) {
+        right++;
+        console.log(right);
+      }
+    }
+var userInput = function(resp, feedback1, feedback2, feedback3, feedback4) {
+  console.log(resp);
+  if ((resp === "yes") || (resp === "Yes") || (resp === "y") || (resp === "Y") || (resp === "YES")) {
+    console.log(feedback1);
+    alert(feedback3);
+  }
+ else {
+    console.log(feedback2);
+    alert(feedback4);
+ }
+}
+
+var game = prompt("Would you like to learn a little about me?");
+      feedback1 = "Lets play a game. I will ask you three questions about me. See how many you can get right.";
+      feedback2 = "Fine dont learn about me. We could of played a fun game!";
+    userInput(game, feedback1, feedback2);
+
+    if ((game === "yes") || (game === "Yes") || (game === "y") || (game === "Y") || (game === "YES")) {
 
       var qu1 = prompt("Was I born in Wisconsin?");
-        if ((qu1 === "yes") || (qu1 === "Yes") || (qu1 === "Y") || (qu1 === "y") || (qu1 === "YES"))  {
-              alert("You answered " + qu1 + ". Sorry I was really born in Denver, Colorado.");
-              console.log("Nope, sorry I was really born in Denver, Colorado.");
-        }
-        else { //if (qu1 === "no") {
-              alert("You answered " + qu1 + ". Great job, I was real born in Denver, Colorado.");
-              console.log("You are correct, I was really born in Denver, Colorado.");
-              right++;
-              console.log(right);
-        }
+          feedback1 = "Nope, sorry I was really born in Denver, Colorado.";
+          feedback2 = "You are correct, I was really born in Denver, Colorado.";
+          feedback3 = "You answered " + qu1 + ". Sorry I was really born in Denver, Colorado.";
+          feedback4 = "You answered " + qu1 + ". Great job, I was real born in Denver, Colorado.";
+        userInput(qu1, feedback1, feedback2, feedback3, feedback4);
+        userNo(qu1);
+
 
       var qu2 = prompt("Is the color of my car Matte Black?");
-        if ((qu2 === "yes") || (qu2 === "Yes") || (qu2 === "Y") || (qu2 === "y") || (qu2 === "YES")) {
-              alert("You answered " + qu2 + ". That's right it is all black.");
-              console.log("That's correct, all black everything.");
-              right++;
-              console.log(right);
-        }
-        else { //if (qu2 === "no") {
-              alert("You answered " + qu2 + ". My car is all black, you should check it out.");
-              console.log("Sorry, but it is. There is a custom finish on it.");
-        }
+          feedback1 = "That's correct, all black everything.";
+          feedback2 = "Sorry, but it is. There is a custom finish on it.";
+          feedback3 = "You answered " + qu2 + ". That's right it is all black.";
+          feedback4 = "You answered " + qu2 + ". My car is all black, you should check it out.";
+        userInput(qu2, feedback1, feedback2, feedback3, feedback4);
+        userYes(qu2);
+
 
       var qu3 = prompt("Is the weight of both my dogs added together over 120 pounds?");
-        if ((qu3 === "yes") || (qu3 === "Yes") || (qu3 === "Y") || (qu3 === "y") || (qu3 === "YES")) {
-              alert("You answered " + qu3 + " and you are correct. They weight 121 pounds.");
-              console.log("That's correct, together they weight 121 pounds.");
-              right++;
-              console.log(right);
-        }
-        else {  //if (qu3 === "no") {
-              alert("You answered " + qu3 + ". Crazy enough they are 121 pounds together.");
-              console.log("Sorry, but it True. Together they weight 121 pounds.");
-        }
+          feedback1 = "That's correct, together they weight 121 pounds.";
+          feedback2 = "Sorry, but it True. Together they weight 121 pounds.";
+          feedback3 = "You answered " + qu3 + " and you are correct. They weight 121 pounds.";
+          feedback4 = "You answered " + qu3 + ". Crazy enough they are 121 pounds together.";
+        userInput(qu3, feedback1, feedback2, feedback3, feedback4);
+        userYes(qu3);
+
 
 alert("You got " + right + " out of 3.");
-  }
-
-else {
-    alert("Fine dont learn about me. We could of played a fun game!");
 }
+
+
+
+
+
+
+
+
 
 
