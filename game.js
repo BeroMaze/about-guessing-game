39ee4be57f72fc19c19f2d35aa1fa39e5743627d
BeroMaze <Berning.corey@gmail.com>
2015-11-10 12:12:54 -0800

    change all questions into var and functions

--- game.js
+++ game.js
@@ -67,49 +67,57 @@ var numResults = function() {
       }
 }
 
-// asking user if the want to play the game
-var game = prompt("Would you like to learn a little about me?");
-      feedback1 = "Lets play a game. I will ask you three questions about me. See how many you can get right.";
-      feedback2 = "Fine dont learn about me. We could of played a fun game!";
-    userInput(game, feedback1, feedback2);
+// // asking user if the want to play the game
+// var game = prompt("Would you like to learn a little about me?");
+//       feedback1 = "Lets play a game. I will ask you three questions about me. See how many you can get right.";
+//       feedback2 = "Fine dont learn about me. We could of played a fun game!";
+//     userInput(game, feedback1, feedback2);
 
-    if ((game === "yes") || (game === "Yes") || (game === "y") || (game === "Y") || (game === "YES")) {
+//     if ((game === "yes") || (game === "Yes") || (game === "y") || (game === "Y") || (game === "YES")) {
 
 //question 1
+var questionB1 = function(){
       var qu1 = prompt("Was I born in Wisconsin?");
           feedback1 = "Nope, sorry I was really born in Denver, Colorado.";
           feedback2 = "You are correct, I was really born in Denver, Colorado.";
           feedback3 = "You answered " + qu1 + ". Sorry I was really born in Denver, Colorado.";
           feedback4 = "You answered " + qu1 + ". Great job, I was real born in Denver, Colorado.";
-        userInput(qu1, feedback1, feedback2, feedback3, feedback4);
+        userInput(qu1, "answer1", feedback1, feedback2, feedback3, feedback4);
         userNo(qu1);
+}
 
 //question 2
+var questionB2 = function(){
       var qu2 = prompt("Is the color of my car Matte Black?");
           feedback1 = "That's correct, all black everything.";
           feedback2 = "Sorry, but it is. There is a custom finish on it.";
           feedback3 = "You answered " + qu2 + ". That's right it is all black.";
           feedback4 = "You answered " + qu2 + ". My car is all black, you should check it out.";
-        userInput(qu2, feedback1, feedback2, feedback3, feedback4);
+        userInput(qu2, "answer2", feedback1, feedback2, feedback3, feedback4);
         userYes(qu2);
+}
 
 //question 3
+var questionB3 = function(){
       var qu3 = prompt("Is the weight of both my dogs added together over 120 pounds?");
           feedback1 = "That's correct, together they weight 121 pounds.";
           feedback2 = "Sorry, but it True. Together they weight 121 pounds.";
           feedback3 = "You answered " + qu3 + " and you are correct. They weight 121 pounds.";
           feedback4 = "You answered " + qu3 + ". Crazy enough they are 121 pounds together.";
-        userInput(qu3, feedback1, feedback2, feedback3, feedback4);
+        userInput(qu3, 'answer3', feedback1, feedback2, feedback3, feedback4);
         userYes(qu3);
+}
 
 //question 4
+var questionB4 = function(){
       var qu4 = prompt("This next question contains some music thought. Is one of the styles of music that I like called Indie?");
           feedback1 = "Sorry Indie is not really my thing.";
           feedback2 = "Thats right. Indie really isnt for me.";
           feedback3 = "You answered " + qu4 + ". Sorry Indie isn't really my style of music.";
           feedback4 = "You answered " + qu4 + ". Thats right Im more of a Electronic music person.";
-        userInput(qu4, feedback1, feedback2, feedback3, feedback4);
+        userInput(qu4, "answer4", feedback1, feedback2, feedback3, feedback4);
         userNo(qu4);
+}
 
 //number guessing game
         numGame();
